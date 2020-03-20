8dede0561c3c7f8ab7706dff06da9180429d1283
stefana-rosca <61684255+stefana-rosca@users.noreply.github.com>
2020-03-20 21:41:36 +0200

    setter getter

--- OOP_lab2/complex.cpp
+++ OOP_lab2/complex.cpp
@@ -7,5 +7,42 @@ using namespace std;
 Complex::Complex()
 {
 	re = 0;
-	im = 0;
+	img = 0;
 }
+
+Complex::Complex(double re, double img)
+{
+	this->re = re;
+	this->img = img;
+}
+
+//destructor
+Complex::~Complex() 
+{
+
+}
+
+//return real part (getter)
+double Complex::getRe() const 
+{
+	return re;
+}
+
+//return imaginary part (getter)
+double Complex::getImg() const
+{
+	return img;
+}
+
+//set real part (setter)
+void Complex::setRe(double re)
+{
+	this->re = re;
+}
+
+//set imaginary part (setter)
+void Complex::setImg(double img)
+{
+	this->img = img;
+}
+
--- OOP_lab2/complex.h
+++ OOP_lab2/complex.h
@@ -1,13 +1,31 @@
 #pragma once
 #include <iostream>
+#include <cmath>
+#include <string>
 using namespace std;
 
 class Complex {
 
 public:
-	//Complex(double re, double im);
+	Complex(double re, double img);
+	Complex();
+	~Complex();
+
+	double getRe() const;      //access method
+	double getImg() const; //access method
+	void setRe(double re);
+	void setImg(double im);
+	Complex add(Complex other);
+	Complex mult(Complex otherone);
+	Complex quot(Complex nr);
+	Complex show_compl();
+	float unghi();
+	double abs();
+	string compute_polar();
+	string show_exp();
+	
 
 private:
 	double re;
-	double im;
+	double img;
 };
