bad937a9b603ae0860be290d57273b5f7e9a5d5a
iuliana02 <iuliana02@users.noreply.github.com>
2020-03-21 23:05:01 +0200

    (...)

--- OOP_lab2/main.cpp
+++ OOP_lab2/main.cpp
@@ -21,8 +21,16 @@ void erstellen()
 
 	Complex r1 = nr1.show_compl();
 	Complex r2 = nr2.show_compl();
-	cout << r1.getRe() << r1.getImg() << "i" << endl;
+	cout << r1.getRe() <<"+"<< r1.getImg() << "i" << endl;
+	cout << r2.getRe() <<"+"<< r2.getImg() << "i" << endl;
 
+
+	Complex nr3(2, 9);
+	Complex r3 = nr3.show_compl();
+	Complex nr4(7, 3.2);
+	Complex r4 = nr4.show_compl();
+	Complex nr5(11.4, 16);
+	Complex r5 = nr5.show_compl();
 }
 
 int main()
